76129757e88b00c4b78be2ef26cbed5250a05151
Andreas Eknes Lie <andrli@equinor.com>
2023-09-21 08:52:42 +0200

    Disable python find package

--- build_dakota.sh
+++ build_dakota.sh
@@ -7,6 +7,7 @@ if [[ -z "${SKIP_SETUP}" ]]; then
   mkdir -p $INSTALL_DIR
 
   yum install wget -y
+  yum install lapack-devel -y
 
   update-alternatives --install /usr/bin/python python /usr/local/bin/python3.11 20
   update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.11 20
@@ -30,6 +31,14 @@ cd build
 export PATH=/tmp/INSTALL_DIR/bin:$PATH
 export LD_LIBRARY_PATH=/tmp/INSTALL_DIR/lib:$LD_LIBRARY_PATH
 
+export PYTHON_ROOT_DIR=$(python3 -c "import sys; print(sys.prefix)")
+export PYTHON_INCLUDE_DIR=$(python -c "import sysconfig; print(sysconfig.get_path('include'))")
+export PYTHON_LIBRARY=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+
+echo $PYTHON_ROOT_DIR
+echo $PYTHON_INCLUDE_DIR
+echo $PYTHON_LIBRARY
+
 cmake \
       -DCMAKE_CXX_STANDARD=14 \
       -DBUILD_SHARED_LIBS=ON \
@@ -42,6 +51,10 @@ cmake \
       -DCMAKE_BUILD_TYPE="Release" \
       -DDAKOTA_NO_FIND_TRILINOS:BOOL=TRUE \
       -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+      -DPython3_INCLUDE_DIRS=$(python -c "import sysconfig; print(sysconfig.get_path('include'))")  \
+      -DPython3_LIBRARIES=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+      -DPYTHON_EXECUTABLE=$(which python) \
+      -DCMAKE_DISABLE_FIND_PACKAGE_Python=TRUE \
       ..
 
 #make -j8 install
