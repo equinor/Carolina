c69618388eaabc6ba7f59257eef383c44d534f48
Andreas Eknes Lie <andrli@equinor.com>
2025-01-20 14:40:47 +0100

    Try to fetch boost from sourceforge instead

--- dakota_manylinux_install_files/build_deps_gha.sh
+++ dakota_manylinux_install_files/build_deps_gha.sh
@@ -30,7 +30,7 @@ yum install -y wget
 cd /tmp
 
 BOOST_VERSION_UNDERSCORES=$(echo $BOOST_VERSION | sed 's/\./_/g')
-wget https://boostorg.jfrog.io/artifactory/main/release/$BOOST_VERSION/source/boost_$BOOST_VERSION_UNDERSCORES.tar.bz2 --no-check-certificate > /dev/null
+wget --quiet https://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION_UNDERSCORES}.tar.bz2 --no-check-certificate > /dev/null
 python_exec=$(which python$1)
 $python_exec -m venv myvenv
 source ./myvenv/bin/activate
