35037767a587b0174ef9b1c7a9515e7669531902
Andreas Eknes Lie <andrli@equinor.com>
2025-03-20 15:30:21 +0100

    Sync docker and gha scripts

--- dakota_manylinux_install_files/build_deps_gha.sh
+++ dakota_manylinux_install_files/build_deps_gha.sh
@@ -8,21 +8,22 @@ fi
 
 cd /tmp
 INSTALL_DIR=/tmp/INSTALL_DIR
+WORKSPACE=/github/workspace
 
 mkdir -p $INSTALL_DIR
-mkdir /github/workspace/trace
-touch /github/workspace/trace/boost_bootstrap.log
-touch /github/workspace/trace/boost_install.log
-touch /github/workspace/trace/dakota_bootstrap.log
-touch /github/workspace/trace/dakota_install.log
-touch /github/workspace/trace/env
+mkdir $WORKSPACE/trace
+touch $WORKSPACE/trace/boost_bootstrap.log
+touch $WORKSPACE/trace/boost_install.log
+touch $WORKSPACE/trace/dakota_bootstrap.log
+touch $WORKSPACE/trace/dakota_install.log
+touch $WORKSPACE/trace/env
 
 # VERY IMPORTANT: extract python dev headers,
 # more info: https://github.com/pypa/manylinux/pull/1250
 pushd /opt/_internal && tar -xJf static-libs-for-embedding-only.tar.xz && popd
 
-echo "pushd /opt/_internal && tar -xJf static-libs-for-embedding-only.tar.xz && popd" >> /github/workspace/trace/env
-echo "INSTALL_DIR=$INSTALL_DIR" >> /github/workspace/trace/env
+echo "pushd /opt/_internal && tar -xJf static-libs-for-embedding-only.tar.xz && popd" >> $WORKSPACE/trace/env
+echo "INSTALL_DIR=$INSTALL_DIR" >> $WORKSPACE/trace/env
 
 yum install lapack-devel -y
 yum install python3-devel.x86_64 -y
@@ -44,45 +45,41 @@ python_root=$(python -c "import sys; print(sys.prefix)")
 python_version=$(python --version | sed -E 's/.*([0-9]+\.[0-9]+)\.([0-9]+).*/\1/')
 python_version_no_dots="$(echo "${python_version//\./}")"
 python_bin_include_lib="    using python : $python_version : $(python -c "from sysconfig import get_paths as gp; g=gp(); print(f\"$python_exec : {g['include']} : {g['stdlib']} ;\")")"
-PYTHON_INCLUDE_DIR="$(python -c "from sysconfig import get_paths as gp; g=gp(); print(f\"{g['include']} \")")"
-PYTHON_LIB_DIR="$(python -c "from sysconfig import get_paths as gp; g=gp(); print(f\"{g['stdlib']} \")")"
 
 echo "Found dev headers $PYTHON_DEV_HEADERS_DIR"
 echo "Found numpy include path $NUMPY_INCLUDE_PATH"
 echo "Found python include path $PYTHON_INCLUDE_PATH"
 echo "Found python root $python_root"
-echo "Found PYTHON_INCLUDE_DIR $PYTHON_INCLUDE_DIR"
-echo "Found PYTHON_LIB_DIR $PYTHON_LIB_DIR"
 
 tar xf boost_$BOOST_VERSION_UNDERSCORES.tar.bz2
 cd boost_$BOOST_VERSION_UNDERSCORES
 
-echo "python_exec=$python_exec" >> /github/workspace/trace/env
-echo "PYTHON_DEV_HEADERS_DIR=$PYTHON_DEV_HEADERS_DIR" >> /github/workspace/trace/env
-echo "NUMPY_INCLUDE_PATH=$NUMPY_INCLUDE_PATH" >> /github/workspace/trace/env
-echo "PYTHON_INCLUDE_PATH=$PYTHON_INCLUDE_PATH" >> /github/workspace/trace/env
-echo "python_root=$python_root" >> /github/workspace/trace/env
-echo "python_version=$python_version" >> /github/workspace/trace/env
-echo "python_version_no_dots=$python_version_no_dots" >> /github/workspace/trace/env
-echo "python_bin_include_lib=$python_bin_include_lib" >> /github/workspace/trace/env
-echo "bootstrap_cmd=./bootstrap.sh --with-libraries=python,filesystem,program_options,regex,serialization,system --with-python=$(which python) --with-python-root=$python_root &> "$INSTALL_DIR/boost_bootstrap.log"" >> /github/workspace/trace/env
+echo "python_exec=$python_exec" >> $WORKSPACE/trace/env
+echo "PYTHON_DEV_HEADERS_DIR=$PYTHON_DEV_HEADERS_DIR" >> $WORKSPACE/trace/env
+echo "NUMPY_INCLUDE_PATH=$NUMPY_INCLUDE_PATH" >> $WORKSPACE/trace/env
+echo "PYTHON_INCLUDE_PATH=$PYTHON_INCLUDE_PATH" >> $WORKSPACE/trace/env
+echo "python_root=$python_root" >> $WORKSPACE/trace/env
+echo "python_version=$python_version" >> $WORKSPACE/trace/env
+echo "python_version_no_dots=$python_version_no_dots" >> $WORKSPACE/trace/env
+echo "python_bin_include_lib=$python_bin_include_lib" >> $WORKSPACE/trace/env
+echo "bootstrap_cmd=./bootstrap.sh --with-libraries=python,filesystem,program_options,regex,serialization,system --with-python=$(which python) --with-python-root=$python_root &> "$INSTALL_DIR/boost_bootstrap.log"" >> $WORKSPACE/trace/env
 
 ./bootstrap.sh --with-libraries=python,filesystem,program_options,regex,serialization,system --with-python=$(which python) --with-python-root="$python_root" &> "$INSTALL_DIR/boost_bootstrap.log"
 sed -i -e "s|.*using python.*|$python_bin_include_lib|" project-config.jam
-echo "# sed -i -e \"s|.*using python.*|$python_bin_include_lib|\" project-config.jam" >> /github/workspace/trace/env
+echo "# sed -i -e \"s|.*using python.*|$python_bin_include_lib|\" project-config.jam" >> $WORKSPACE/trace/env
 
-./b2 install -j8 -a cxxflags="-std=c++17" --prefix="$INSTALL_DIR" &> /github/workspace/trace/boost_install.log
-echo "# ./b2 install -j8 -a cxxflags="-std=c++17" --prefix="$INSTALL_DIR" &> /github/workspace/trace/boost_install.log" >> /github/workspace/trace/env
+./b2 install -j8 -a cxxflags="-std=c++17" --prefix="$INSTALL_DIR" &> $WORKSPACE/trace/boost_install.log
+echo "# ./b2 install -j8 -a cxxflags="-std=c++17" --prefix="$INSTALL_DIR" &> $WORKSPACE/trace/boost_install.log" >> $WORKSPACE/trace/env
 
 cd $INSTALL_DIR
 DAKOTA_INSTALL_DIR=/tmp/INSTALL_DIR/dakota
 mkdir -p $DAKOTA_INSTALL_DIR
-echo "DAKOTA_INSTALL_DIR=$DAKOTA_INSTALL_DIR" >> /github/workspace/trace/env
+echo "DAKOTA_INSTALL_DIR=$DAKOTA_INSTALL_DIR" >> $WORKSPACE/trace/env
 
 wget https://github.com/snl-dakota/dakota/releases/download/v$DAKOTA_VERSION/dakota-$DAKOTA_VERSION-public-src-cli.tar.gz > /dev/null
 tar xf dakota-$DAKOTA_VERSION-public-src-cli.tar.gz
 
-CAROLINA_DIR=/github/workspace
+CAROLINA_DIR=$WORKSPACE
 cd dakota-$DAKOTA_VERSION-public-src-cli
 
 patch -p1 < $CAROLINA_DIR/dakota_manylinux_install_files/CMakeLists.txt.patch
@@ -95,10 +92,12 @@ cd build
 export PATH=/tmp/INSTALL_DIR/bin:$PATH
 export PYTHON_INCLUDE_DIRS="$PYTHON_INCLUDE_PATH $PYTHON_DEV_HEADERS_DIR /tmp/INSTALL_DIR/lib"
 export PYTHON_EXECUTABLE=$(which python)
+export LD_LIBRARY_PATH="$INSTALL_DIR/lib:/usr/local/lib:$PYTHON_INCLUDE_PATH:$NUMPY_INCLUDE_PATH:$NUMPY_INCLUDE_PATH/numpy:$PYTHON_DEV_HEADERS_DIR:/tmp/INSTALL_DIR/lib:$LD_LIBRARY_PATH"
 
-echo "export PATH=$PATH" >> /github/workspace/trace/env
-echo "export PYTHON_INCLUDE_DIRS=$PYTHON_INCLUDE_DIRS" >> /github/workspace/trace/env
-echo "export PYTHON_EXECUTABLE=$PYTHON_EXECUTABLE" >> /github/workspace/trace/env
+echo "export PATH=$PATH" >> $WORKSPACE/trace/env
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $WORKSPACE/trace/env
+echo "export PYTHON_INCLUDE_DIRS=$PYTHON_INCLUDE_DIRS" >> $WORKSPACE/trace/env
+echo "export PYTHON_EXECUTABLE=$PYTHON_EXECUTABLE" >> $WORKSPACE/trace/env
 
 export BOOST_PYTHON="boost_python$python_version_no_dots"
 export BOOST_ROOT=$INSTALL_DIR
@@ -106,23 +105,22 @@ export PATH="$PATH:$INSTALL_DIR/bin"
 
 # More stable approach: Go via python
 numpy_lib_dir=$(find /tmp/myvenv/ -name numpy.libs)
-export LD_LIBRARY_PATH="$PYTHON_LIB_DIR:/usr/lib:/usr/lib64:$INSTALL_DIR/lib:$INSTALL_DIR/bin:$numpy_lib_dir:$NUMPY_INCLUDE_PATH:$PYTHON_DEV_HEADERS_DIR"
+export LD_LIBRARY_PATH="/usr/lib:/usr/lib64:$INSTALL_DIR/lib:$INSTALL_DIR/bin:$numpy_lib_dir:$NUMPY_INCLUDE_PATH"
 export CMAKE_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed 's/::/:/g' | sed 's/:/;/g')
 export PYTHON_LIBRARIES="/usr/lib64/"
-export PYTHON_INCLUDE_DIR="/usr/include/python3.6m"
-
+export PYTHON_INCLUDE_DIR="/opt/_internal/cpython-3.7.17/include/python3.7m"
 export CMAKE_LINK_OPTS="-Wl,--copy-dt-needed-entries,-l pthread"
 
 export PYTHON_INCLUDE_DIR=$PYTHON_DEV_HEADERS_DIR
 
-echo "export BOOST_PYTHON=$BOOST_PYTHON" >> /github/workspace/trace/env
-echo "export BOOST_ROOT=$BOOST_ROOT" >> /github/workspace/trace/env
-echo "export PATH=$PATH" >> /github/workspace/trace/env
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> /github/workspace/trace/env
-echo "export CMAKE_LIBRARY_PATH=\"$CMAKE_LIBRARY_PATH\"" >> /github/workspace/trace/env
-echo "export PYTHON_LIBRARIES=\"$PYTHON_LIBRARIES\"" >> /github/workspace/trace/env
-echo "export PYTHON_INCLUDE_DIR=\"$PYTHON_INCLUDE_DIR\"" >> /github/workspace/trace/env
-echo "export CMAKE_LINK_OPTS=\"$CMAKE_LINK_OPTS\"" >> /github/workspace/trace/env
+echo "export BOOST_PYTHON=$BOOST_PYTHON" >> $WORKSPACE/trace/env
+echo "export BOOST_ROOT=$BOOST_ROOT" >> $WORKSPACE/trace/env
+echo "export PATH=$PATH" >> $WORKSPACE/trace/env
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $WORKSPACE/trace/env
+echo "export CMAKE_LIBRARY_PATH=\"$CMAKE_LIBRARY_PATH\"" >> $WORKSPACE/trace/env
+echo "export PYTHON_LIBRARIES=\"$PYTHON_LIBRARIES\"" >> $WORKSPACE/trace/env
+echo "export PYTHON_INCLUDE_DIR=\"$PYTHON_INCLUDE_DIR\"" >> $WORKSPACE/trace/env
+echo "export CMAKE_LINK_OPTS=\"$CMAKE_LINK_OPTS\"" >> $WORKSPACE/trace/env
 
 cmake_command="""
 cmake \
@@ -143,20 +141,22 @@ cmake \
       .. &> "$INSTALL_DIR/dakota_bootstrap.log"
 
 """
-echo "# $cmake_command" >> /github/workspace/trace/env
+echo "# $cmake_command" >> $WORKSPACE/trace/env
 
 echo "Boostrapping Dakota ..."
-$($cmake_command &> /github/workspace/trace/dakota_bootstrap.log)
+$($cmake_command &> $WORKSPACE/trace/dakota_bootstrap.log)
 
-echo "# make --debug=b -j8 install" >> /github/workspace/trace/env
+echo "# make --debug=b -j8 install" >> $WORKSPACE/trace/env
 echo "Building Dakota ..."
-make --debug=b -j8 install &> /github/workspace/trace/dakota_install.log
+make --debug=b -j8 install &> $WORKSPACE/trace/dakota_install.log
 
 
-DEPS_BUILD=/github/workspace/deps_build
+DEPS_BUILD=$WORKSPACE/deps_build
 
 mkdir -p $DEPS_BUILD
 
 cp -r $INSTALL_DIR/bin $DEPS_BUILD
 cp -r $INSTALL_DIR/lib $DEPS_BUILD
 cp -r $INSTALL_DIR/include $DEPS_BUILD
+
+env
